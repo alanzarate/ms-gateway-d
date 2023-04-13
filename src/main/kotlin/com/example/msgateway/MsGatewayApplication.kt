43ea87988e0501ed5068d754b942974ea0959285
alanzarate <alnzarate@gmail.com>
2023-04-13 11:16:47 -0400

    first commit

---
+++ src/main/kotlin/com/example/msgateway/MsGatewayApplication.kt
@@ -0,0 +1,13 @@
+package com.example.msgateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+
+@SpringBootApplication
+@EnableDiscoveryClient
+class MsGatewayApplication
+
+fun main(args: Array<String>) {
+	runApplication<MsGatewayApplication>(*args)
+}
